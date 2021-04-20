41ef3ae0f3d0a4dbf5ef9934e7d087ab3f794d26
Sam Smith <sam@example.com>
2021-04-20 11:50:33 +0200

    commit

--- client/src/components/MobileNavbar.jsx
+++ client/src/components/MobileNavbar.jsx
@@ -4,7 +4,7 @@ function MobileNavbar() {
           <ul>
               <li><a href="/"><i className="fas fa-home"></i></a></li>
               <li><a href="/"><i className="fas fa-search"></i></a></li>
-              <li><button><i className="fas fa-plus"></i></button></li>
+              <li><button><i class="fas fa-plus"></i></button></li>
               <li><a href="/"><i className="far fa-comment"></i></a></li>
               <li><a href="/"><i className="fas fa-user"></i></a></li>
           </ul>
