44512a244681419e58d391bba2da75d8e44b8e55
szatgabor <gabor.samers@gmail.com>
2021-05-10 08:44:33 +0200

    Router style structurised

--- client/src/components/MobileHome.jsx
+++ client/src/components/MobileHome.jsx
@@ -1,36 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-// const LoginButton = styled(Link)`
-//   color: #000;
-//   background-color: #fff;
-//   &:active{
-//     background-color: #000;
-//     color: #fff;
-//     border: 3px solid #000`;
-
-// const RegButton = styled(Link)`
-//   color: #fff;
-//   background-color: #000;
-//   &:active {
-//     color: #000;
-//     background-color: #fff;
-//     border: 3px solid #000`;
-
-// const commonButtonStyle = {
-//   width: "49%",
-//   height: "50px",
-//   transition: "background 400ms",
-//   textTransform: "uppercase",
-//   textDecoration: "none",
-//   borderRadius: "8px",
-//   border: "solid 2.5px",
-//   fontWeight: "600",
-//   display: "flex",
-//   justifyContent: "center",
-//   alignItems: "center",
-// };
 
 function MobileHome() {
   return (
