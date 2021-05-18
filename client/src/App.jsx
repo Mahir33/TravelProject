1f68480c80dfabbf5828f28ca0b8b54aaf891ef0
matheusgleizer <matheusgleizer@gmail.com>
2021-05-18 16:09:41 +0200

    register bug fixed

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../src/css/main.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import MobilIndex from "./components/MobileIndex";
+import MobilLogin from "./components/MobileLogin";
+import MobilRegister1 from "./components/MobileRegister1";
+import MobileHome from "./components/MobileHome";
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={MobilIndex} />
+      <Route path="/login" component={MobilLogin} />
+      <Route path="/register" component={MobilRegister1} />
+      <Route path="/home" component={MobileHome} />
+    </Switch>
+  </BrowserRouter>
+);
+
+function App() {
+  return <>{routes}</>;
+}
+
+export default App;
