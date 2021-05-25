43616c28bc543f1ea43ba1c2826eb02c779675ec
Daniel Ulises <daniel.cau@pm.me>
2021-05-25 14:30:00 +0200

    Finalized merging

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,18 +1,10 @@
 const User = require("../models/userModels");
 const bcrypt = require("bcrypt");
-const {
-  dataUri
-} = require("../middleware/multerMiddleware");
-const {
-  uploader
-} = require("cloudinary");
+const { dataUri } = require("../middleware/multerMiddleware");
+const { uploader } = require("cloudinary");
 
 const signup = async (req, res) => {
-  const {
-    email,
-    username,
-    password
-  } = req.body;
+  const { email, username, password } = req.body;
 
   try {
     const user = await User.create({
@@ -31,25 +23,17 @@ const signup = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const {
-    email,
-    password
-  } = req.body;
+  const { email, password } = req.body;
 
   try {
     const user = await User.findOne({
-<<<<<<< HEAD
       email,
-=======
-      email
->>>>>>> b2b60e55387256f793c1e356f86bc2a58cdaa331
     });
 
     if (user) {
       const auth = await bcrypt.compare(password, user.password);
 
       if (auth) {
-<<<<<<< HEAD
         res.json({ message: "Successfully logged in!", user });
       }
 
@@ -57,28 +41,11 @@ const login = async (req, res) => {
     }
 
     res.json({ message: "E-mail does not exist!" });
-=======
-        res.status(200).json({
-          message: "Successfully logged in!",
-          user
-        });
-      }
-
-      res.status(402).json({
-        message: "Incorrect password!"
-      });
-    }
-
-    res.status(402).json({
-      message: "E-mail does not exist!"
-    });
->>>>>>> b2b60e55387256f793c1e356f86bc2a58cdaa331
   } catch (err) {}
 };
 
 // Temporary testing
 const getUser = async (req, res) => {
-<<<<<<< HEAD
   try {
     const { username } = req.params;
     const user = await User.findOne({ username });
@@ -86,23 +53,10 @@ const getUser = async (req, res) => {
   } catch (err) {
     res.json(err);
   }
-=======
-
-  const {
-    username
-  } = req.params;
-  const user = await User.findOne({
-    username
-  });
-  console.log(user);
-  res.send({
-    user
-  });
->>>>>>> b2b60e55387256f793c1e356f86bc2a58cdaa331
 };
 
 module.exports = {
   signup,
   login,
-  getUser
+  getUser,
 };
