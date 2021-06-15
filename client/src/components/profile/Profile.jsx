7ef3778e58e9c6097dfde2d17f70364b15555f9e
matheusgleizer <matheusgleizer@gmail.com>
2021-06-15 13:36:54 +0200

    implementing token in the header

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useContext } from "react";
-import { PropContainer } from "../../PropContainer";
-import { FiSettings } from "react-icons/fi";
-import { FaRegArrowAltCircleLeft } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { Input } from "semantic-ui-react";
+import React, {useEffect, useContext} from "react";
+import {PropContainer} from "../../PropContainer";
+import {FiSettings} from "react-icons/fi";
+import {FaRegArrowAltCircleLeft} from "react-icons/fa";
+import {Link} from "react-router-dom";
+import {Input} from "semantic-ui-react";
 
 function Profile() {
   const {
@@ -19,6 +19,9 @@ function Profile() {
   const fetchProfile = async () => {
     const req = await fetch(`http://localhost:3001/user/${username}`, {
       method: "get",
+      headers: {
+        "x-access-token": sessionStorage.getItem("token"),
+      },
     })
       .then((res) => res.json())
       .then((json) => {
@@ -37,93 +40,92 @@ function Profile() {
 
   let displayProfile = (
     <div>
-      <div className="profile-container">
-        <div className="back">
-          <Link to="/home">
+      <div className='profile-container'>
+        <div className='back'>
+          <Link to='/home'>
             <FaRegArrowAltCircleLeft />
           </Link>
         </div>
-        <div className="search-input">
+        <div className='search-input'>
           <Input
-            icon="search"
-            placeholder="Search..."
-            className="semantic-input"
+            icon='search'
+            placeholder='Search...'
+            className='semantic-input'
           />
         </div>
-        <div className="profile-settings-button">
-          <Link to="/profile-settings">
+        <div className='profile-settings-button'>
+          <Link to='/profile-settings'>
             <FiSettings />
           </Link>
         </div>
       </div>
-      <div className="profile-display">
-        <div className="profile-picture-container">
+      <div className='profile-display'>
+        <div className='profile-picture-container'>
           <div
-            className="profile-image"
+            className='profile-image'
             style={{
               backgroundImage: `url(${picture})`,
-            }}
-          ></div>
+            }}></div>
         </div>
         <h2>{username}</h2>
         <h6>{location}</h6>
-        <button className="follow-btn">Follow</button>
+        <button className='follow-btn'>Follow</button>
         <button>Message</button>
-        <div className="album">
+        <div className='album'>
           <img
-            src="https://source.unsplash.com/collection/190727/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/190727/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/190728/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/190728/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/1907200/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/1907200/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/190726/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/190726/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/190723/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/190723/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/1907289/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/1907289/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/190727/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/190727/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/190728/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/190728/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/1907200/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/1907200/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/190726/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/190726/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/190723/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/190723/400x500'
+            alt='profile'
           />
           <img
-            src="https://source.unsplash.com/collection/1907289/400x500"
-            alt="profile"
+            src='https://source.unsplash.com/collection/1907289/400x500'
+            alt='profile'
           />
         </div>
       </div>
     </div>
   );
 
-  return <div className="profile">{displayProfile}</div>;
+  return <div className='profile'>{displayProfile}</div>;
 }
 
 export default Profile;
