cc454ab7987aecb70c0c8865b8639f4c119332a1
matheusgleizer <matheusgleizer@gmail.com>
2021-06-23 13:31:54 +0200

    nav bar added to MobileHome

--- client/src/components/MobileHome/MobileHome.jsx
+++ client/src/components/MobileHome/MobileHome.jsx
@@ -3,9 +3,11 @@ import NewPicScroller from "./_components/NewPicScroller";
 import BrowseAllGallery from "./_components/BrowseAllGallery";
 import HomeHeader from "./_components/HomeHeader";
 import BrowseHeader from "./_components/BrowseHeader";
+import ProfileNavBar from "../ProfileNavbar/ProfileNavbar";
 function MobileHome({username, setUsername}) {
   return (
     <div className="home-container">
+      <ProfileNavBar />
       <HomeHeader />
       <NewPicScroller />
       <BrowseHeader />
